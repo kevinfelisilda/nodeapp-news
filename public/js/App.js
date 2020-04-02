f1014b64310bf9efd5fe31c85cd00c702800d4a2
kevinfelisilda <kevinfelisilda@gmail.com>
2020-04-02 20:26:56 +0800

    add react client

---
+++ public/js/App.js
@@ -0,0 +1,61 @@
+const query = `
+  query getNews {
+    news {
+      id
+      title
+      link
+      snippet
+      date
+    }
+  }
+`;
+
+const getNews = () => window.fetch('/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query })
+}).then(response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    throw new Error(response.statusText);
+  }
+}).then(response => response.json()).then(({ data }) => data.news);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    getNews().then(news => {
+      this.setState({
+        news,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    const { news } = this.state;
+    return (
+      <div>
+        <h2>News:</h2>
+        <div>
+          {news.map((item) => (
+            <div key={item.id} className="news-item">
+              <small>{item.date.substring(0, item.date.length - 13)}</small>
+              <a href={item.link}><h5>{item.title}</h5></a>
+              <p>{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
